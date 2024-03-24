f13c4f4896e143f8547100a6f5f11d00f3332f1c
FAU-ML202324 <151638520+FAU-ML202324@users.noreply.github.com>
2024-03-24 20:45:53 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,6 +9,19 @@ def main():
     # Abschnitt für SelectSlider-Elemente
     st.header("Wählen Sie die Mengen Ihrer Betoninhaltsstoffe aus")
 
+    st.title('Tool Wear Detection App')
+
+    # Add a radio button or selectbox for model selection
+    model_choice = st.sidebar.radio('Choose Model', ('Large Model', 'Small Model'))
+
+    st.sidebar.title('Enter Data')
+    machine_name = st.sidebar.text_input('Machine Name')
+    work_cycle = st.sidebar.text_input('Work Cycle')
+    speed = st.sidebar.number_input('Speed', value=0)
+    feed = st.sidebar.number_input('Feed', value=0)
+
+    uploaded_image = st.file_uploader('Upload an image of the tool', type=['jpg', 'png'])
+
     # Variablen und ihre Bereichsgrenzen
     variables = {
         "cement": (100, 500),
