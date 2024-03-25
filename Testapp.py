5a1fdef51bcafb10d0baf78937190db965f7d07c
FAU-ML202324 <151638520+FAU-ML202324@users.noreply.github.com>
2024-03-25 02:17:00 +0100

    Update Testapp.py

--- Testapp.py
+++ Testapp.py
@@ -8,16 +8,7 @@ filename = 'model_augment_quiteokay.pkl'
 large_model = joblib.load(filename)
 
 
-st.title('Tool Wear Detection App')
 
-# Add a radio button or selectbox for model selection
-model_choice = st.sidebar.radio('Choose Model', ('Large Model', 'Small Model'))
-
-st.sidebar.title('Enter Data')
-machine_name = st.sidebar.text_input('Machine Name')
-work_cycle = st.sidebar.text_input('Work Cycle')
-speed = st.sidebar.number_input('Speed', value=0)
-feed = st.sidebar.number_input('Feed', value=0)
 
 uploaded_image = st.file_uploader('Upload an image of the tool', type=['jpg', 'png'])
 left = 125
@@ -47,25 +38,39 @@ def max_index(modelpred):
   max_ind = max(enumerate(a),key=lambda x: x[1])[0]
   return max_ind
 
+def main():
 
-if uploaded_image is not None:
-    image = Image.open(uploaded_image)
-    #st.image(image, caption='Uploaded Image', use_column_width=True)
-    cropped_image = image.crop((left, upper, right, lower))
-    resized_image = cropped_image.resize((cropped_image.width // 2, cropped_image.height // 2))
-    bild=[]
-    bild.append(resized_image)
-    image_array = np.asarray(bild)
+    st.title('Tool Wear Detection App')
 
-if st.button("Werkzeugzustand bewerten"):
-    if model_choice == 'Large Model':
-        modelprediction, confidence = predict_tool_wear_large(image_array)
-        st.write("Werkzeugzustand:")
-        st.text_area("Ergebnis", f"{modelprediction}", height=100)
-        st.write("Wie sicher ist sich das Modell bei dieser Klassifizierung:")
-        st.text_area(f"{confidence}", height=100)
-    elif model_choice == 'Small Model':
-        toolwear_prediction = predict_tool_wear_small(image_array)
-        prediction = predict_tool_wear_large(image_array)
-        st.write("Werkzeugzustand:")
-        st.text_area("Ergebnis", f"{toolwear_prediction}", height=100)
+    # Add a radio button or selectbox for model selection
+    model_choice = st.sidebar.radio('Choose Model', ('Large Model', 'Small Model'))
+
+    st.sidebar.title('Enter Data')
+        machine_name = st.sidebar.text_input('Machine Name')
+    work_cycle = st.sidebar.text_input('Work Cycle')
+    speed = st.sidebar.number_input('Speed', value=0)
+    feed = st.sidebar.number_input('Feed', value=0)
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        #st.image(image, caption='Uploaded Image', use_column_width=True)
+        cropped_image = image.crop((left, upper, right, lower))
+        resized_image = cropped_image.resize((cropped_image.width // 2, cropped_image.height // 2))
+        bild=[]
+        bild.append(resized_image)
+        image_array = np.asarray(bild)
+
+    if st.button("Werkzeugzustand bewerten"):
+        if model_choice == 'Large Model':
+            modelprediction, confidence = predict_tool_wear_large(image_array)
+            st.write("Werkzeugzustand:")
+            st.text_area("Ergebnis", f"{modelprediction}", height=100)
+            st.write("Wie sicher ist sich das Modell bei dieser Klassifizierung: ", f"{confidence}"))
+        elif model_choice == 'Small Model':
+            toolwear_prediction = predict_tool_wear_small(image_array)
+            prediction = predict_tool_wear_large(image_array)
+            st.write("Werkzeugzustand:")
+            st.text_area("Ergebnis", f"{toolwear_prediction}", height=100)
+
+
+if __name__ == "__main__":
+    main()
